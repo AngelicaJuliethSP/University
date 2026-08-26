75d63bc1b131fe25d7fee1c22453b2501c61fe45
AngelicaJuliethSP <angelica.silva01@globant.com>
2026-08-25 21:24:51 -0500

    Creación de la clase Teacher

--- src/model/Teacher.java
+++ src/model/Teacher.java
@@ -1,4 +1,21 @@
 package model;
 
-public class Teacher {
+public abstract class Teacher extends Person {
+
+    private double baseSalary;
+
+    public Teacher(String name, double baseSalary) {
+        super(name);
+        this.baseSalary = baseSalary;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public abstract double calculateSalary();
 }
