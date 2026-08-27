432cf544a15bccb136a4359662e1cf72c4c6d47c
AngelicaJuliethSP <angelica.silva01@globant.com>
2026-08-27 10:35:18 -0500

    Establece salaario para Full Time Teacher

---
+++ src/model/FullTimeTeacher.java
@@ -0,0 +1,25 @@
+package model;
+
+public class FullTimeTeacher extends Teacher {
+
+    private int experienceYears;
+
+    public FullTimeTeacher(String name, double baseSalary, int experienceYears) {
+        super(name, baseSalary);
+        this.experienceYears = experienceYears;
+    }
+
+    public int getExperienceYears() {
+        return experienceYears;
+    }
+
+    public void setExperienceYears(int experienceYears) {
+        this.experienceYears = experienceYears;
+    }
+
+    @Override
+    public  double  calculateSalary(){
+        return getBaseSalary()*(1.10*experienceYears);
+    }
+
+}
